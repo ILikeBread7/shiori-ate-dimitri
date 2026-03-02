fffaccf41688ff23849ad7c7b4068b32c2d182fa
ILikeBread7 <pjaworski47@gmail.com>
2026-03-02 18:20:26 +0100

    After deploy script

---
+++ dev/after_deploy.sh
@@ -0,0 +1,10 @@
+rm /tmp/out/shiori/www/audio/bgm/{Battle,Castle,Ship}*
+rm /tmp/out/shiori/www/audio/me/{Defeat,Gameover,Victory}*
+rm /tmp/out/shiori/www/audio/se/{Run,Shop}*
+rm -rf /tmp/out/shiori/www/dev
+rm /tmp/out/shiori/www/img/battlebacks*/*
+rm /tmp/out/shiori/www/translationengine.html
+
+cp /home/paul/rpgmaker/shiori/img/system/{bgsprite,title_image,Window_}* /tmp/out/shiori/www/img/system
+cp /home/paul/rpgmaker/shiori/img/titles1/Shiori_full.png /tmp/out/shiori/www/img/titles1
+cp /home/paul/rpgmaker/shiori/audio/se/{Computer,Crow}* /tmp/out/shiori/www/audio/se
