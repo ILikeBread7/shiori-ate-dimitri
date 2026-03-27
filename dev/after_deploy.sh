b91125e19f0da32a27b018665fc59db73b8f6da7
ILikeBread7 <pjaworski47@gmail.com>
2026-03-27 15:49:41 +0100

    Added removing Game.desktop~ backup to after_deploy.sh

--- dev/after_deploy.sh
+++ dev/after_deploy.sh
@@ -5,6 +5,9 @@ rm -rf /tmp/out/shiori/www/dev
 rm /tmp/out/shiori/www/img/battlebacks*/*
 rm /tmp/out/shiori/www/translationengine.html
 
+# Linux game executable backup file
+rm /tmp/out/shiori/Game.desktop~ 2>/dev/null
+
 cp /home/paul/rpgmaker/shiori/img/system/{bgsprite,title_image,Window_}* /tmp/out/shiori/www/img/system
 cp /home/paul/rpgmaker/shiori/img/titles1/Shiori_full.png /tmp/out/shiori/www/img/titles1
 cp /home/paul/rpgmaker/shiori/audio/se/{Computer,Crow}* /tmp/out/shiori/www/audio/se
